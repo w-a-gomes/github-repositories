4ef6d8e365c7d9594ffa88453982de087e1cc2e2
w-a-gomes <wildenalisson@outlook.com>
2022-03-29 23:49:40 -0300

    Django app - index

--- gittools/gittools/urls.py
+++ gittools/gittools/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('searchapp/', include('searchapp.urls')),
+    path('', include('searchapp.urls')),
 ]
