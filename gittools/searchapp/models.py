f00372103ffe817d0adb92985dbe4890d76e390a
w-a-gomes <wildenalisson@outlook.com>
2022-04-02 01:05:59 -0300

    Details and description

--- gittools/searchapp/models.py
+++ gittools/searchapp/models.py
@@ -5,6 +5,9 @@ class GitRepo(models.Model):
     name = models.CharField(max_length=200)
     _id = models.IntegerField(default=0)
     lang = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, null=True)
+    stargazers_count = models.CharField(max_length=200, null=True)
+    html_url = models.CharField(max_length=200, null=True)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
--- gittools/searchapp/views.py
+++ gittools/searchapp/views.py
@@ -9,7 +9,7 @@ from .models import GitRepo
 
 def index(request):
     programming_lang = None
-    git_response_dict = None
+    git_response = []
 
     if request.method == 'POST':
 
@@ -23,14 +23,32 @@ def index(request):
                         .format(programming_lang)
                     )
                     response = requests.get(url)
-                    git_response_dict = response.json()
+                    response_dict = response.json()
+
+                    # Show all keys | {'items': [{}, {}, {}], ...}
+                    # for i in git_response_dict['items'][0].keys():
+                    #     print(i)
+
+                    for repo in response_dict['items']:
+                        if repo['description']:
+                            description = repo['description'] if len(repo['description']) < 75 else repo['description'][:72] + '...'
+                        else:
+                            description = 'Without description'
+                        
+                        git_response.append({
+                            'id': repo['id'],
+                            'name': repo['name'],
+                            'stargazers_count': repo['stargazers_count'],
+                            'description': description,
+                            'html_url': repo['html_url'],
+                        })
 
     return render(
         request,
         'searchapp/index.html',
         {
             'programming_lang': programming_lang,
-            'git_response_dict': git_response_dict,
+            'git_response': git_response,
             'repo_save': False,
             'existing_repositories': [x.name for x in GitRepo.objects.all()],
         }
@@ -45,8 +63,8 @@ def infosave(request):
         if 'save_button_pressed' in request.POST:
 
             for request_post in request.POST:
-                if 'repository//' in request_post[:len('repository//')]:
-                    _repo_prefix, repo_id, repo_name, repo_lang = request_post.split('//')
+                if 'repository✱' in request_post[:len('repository✱')]:
+                    _prefix, repo_id, repo_name, repo_lang, repo_description, repo_stargazers_count, repo_html_url = request_post.split('✱')
                     
                     # Check duplicates and save
                     if repo_name not in existing_repositories:
@@ -54,6 +72,9 @@ def infosave(request):
                             name=repo_name,
                             _id=int(repo_id),
                             lang=repo_lang,
+                            description=repo_description,
+                            stargazers_count=repo_stargazers_count,
+                            html_url=repo_html_url,
                             pub_date=timezone.now())
                         gr.save()
                         repo_save = True
@@ -74,7 +95,20 @@ def saved(request):
         for repo in GitRepo.objects.all():
             if repo.lang == language:
                 space_decoration = ('__________')[:10 - len(repo.lang)]
-                existing_repositories.append((repo.lang, space_decoration, repo.name))
+                space_description = '_' * 11
+                description = repo.description if len(repo.description) < 60 else repo.description[:57] + '...'
+                existing_repositories.append(
+                    {
+                        'id': repo.id,
+                        'lang': repo.lang,
+                        'space_decoration': space_decoration,
+                        'name': repo.name,
+                        'space_description': space_description,
+                        'description': description,
+                        'stargazers_count': repo.stargazers_count,
+                        'html_url': repo.html_url,
+                    }
+                )
 
     return render(request, 'searchapp/saved.html', {
         'existing_repositories': existing_repositories,
@@ -87,8 +121,8 @@ def inforemove(request):
         if 'remove_button_pressed' in request.POST:
 
             for request_post in request.POST:
-                if 'repository//' in request_post[:len('repository//')]:
-                    _repository_prefix, repository_name = request_post.split('//')
+                if 'repository✱' in request_post[:len('repository✱')]:
+                    _repository_prefix, repository_name = request_post.split('✱')
 
                     # Remove
                     gr = GitRepo.objects.get(name=repository_name)
