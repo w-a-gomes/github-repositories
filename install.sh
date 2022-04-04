30c2954ee6801794f9330732c23b891a31bf28ea
w-a-gomes <wildenalisson@outlook.com>
2022-04-04 20:58:54 -0300

    start app

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-sudo apt install -y python3 python3-pip python3-venv git docker docker-compose
-
-# Download
-git clone https://github.com/w-a-gomes/dev-hiring-challenge.git && cd dev-hiring-challenge/
 sudo groupadd docker ; sudo usermod -aG docker $USER ; docker-compose up -d
 
 python3 -m pip install --upgrade pip && python3 -m venv venv && . venv/bin/activate
@@ -12,5 +8,26 @@ python -m pip install psycopg2-binary && python -m pip install -r requirements.t
 mkdir -p db/postgresql/data/ && cd gittools/
 python manage.py makemigrations && python manage.py migrate
 
-python manage.py runserver
-# coverage run --source='.' manage.py test searchapp && coverage report
+clear
+python3 - << EOF
+s = """
++-------------------------------------------------------+
+|                                                       |
+|                                                       |
+|                                                       |
+|                                                       |
+|                                                       |
+|             Run the 'start-app' script                |
+|                                                       |
+|                    ./start-app                        |
+|                                                       |
+|    Go to 'http://127.0.0.1:8000' in your browser.     |
+|                                                       |
+|                                                       |
+|                                                       |
+|                                                       |
+|                                                       |
++-------------------------------------------------------+
+"""
+print(s)
+EOF
